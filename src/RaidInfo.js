b23d8c50ddef2a1fa61b7c8f6c840839a07536c5
Jason Runta <jasonr@accretivetg.com>
2018-02-09 09:57:19 -0800

    * google maps woot woot

--- src/RaidInfo.js
+++ src/RaidInfo.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 export default class RaidInfo extends Component {
   render() {
-    return null;
+    return (
+      <img src="https://maps.googleapis.com/maps/api/streetview?size=400x200&location=47.5609328,-122.3757268
+      &fov=120&heading=10&pitch=10
+    &key=AIzaSyDyP9AjluBnbeEGrVd63-nDXcAf_6QqDiU" />
+    );
   }
 }
